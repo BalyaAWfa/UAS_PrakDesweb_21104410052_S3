8083952b0ee3829f8388c9a304e69a297aac47a4
Balya_Ahmad <65434116+Balyax@users.noreply.github.com>
2023-01-30 14:36:23 +0700

    Add files via upload

---
+++ Soal Uas 3/soal3.js
@@ -0,0 +1,10 @@
+function input(){
+    var Nama = String(document.fform.Nama.value);
+    document.fform.namaLo.value = Nama;
+
+    var Nim = String(document.fform.Nim.value);
+    document.fform.nimLo.value = Nim;
+
+    var Kelas = String(document.fform.Kelas.value);
+    document.fform.kelasLo.value = Kelas; 
+}
